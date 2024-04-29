c81955656bfaa0df6cd4b3e613a9dafb558b9d48
vitroid <vitroid@gmail.com>
2024-04-29 18:00:03 +0900

    -

--- graphstat/__init__.py
+++ graphstat/__init__.py
@@ -5,43 +5,49 @@ __version__ = "0.2"
 
 #!/usr/bin/env python
 
-#wrapper for mysql.
+# wrapper for mysql.
 
 import networkx as nx
 import numpy as np
 import logging
 import hashlib
 
+
 def encode_graph(g):
     return ",".join([line for line in nx.generate_edgelist(g, data=False)])
 
+
 def decode_graph(s):
     return nx.parse_edgelist(s.split(","))
+
 
 def matrix_sort(m):
     newm = []
     for row in m:
         n = 0
-        for x in np.sort(row): # sorted copy
-            n = n*10 + int(x)
+        for x in np.sort(row):  # sorted copy
+            n = n * 10 + int(x)
         newm.append(n)
     return " ".join([str(x) for x in sorted(newm)])
 
 
 def sorteddm(g):
     logger = logging.getLogger()
-    result = hashlib.md5(matrix_sort(np.array(nx.floyd_warshall_numpy(g))).encode('utf-8')).hexdigest()
+    result = hashlib.md5(
+        matrix_sort(np.array(nx.floyd_warshall_numpy(g))).encode("utf-8")
+    ).hexdigest()
     logger.debug(result)
     return result
 
 
-#local, volatile set of graphs.
-class GraphStat():
+# local, volatile set of graphs.
+class GraphStat:
     def __init__(self):
         # lookup table
         self.graphhashes = dict()
         # storage
-        self.graphs     = []
+        self.graphs = []
+
     def query_id(self, a):
         assert nx.is_connected(a)
         self.lastgraph = a
@@ -52,12 +58,13 @@ class GraphStat():
                 # 実際にはこれまでconflictは一度も起きていないので、
                 # 確認する必要はないかもしれないが、30%のコストで
                 # 安全になるなら当面はこのままでいこう。
-                if nx.is_isomorphic(self.graphs[g_id],a):
+                if nx.is_isomorphic(self.graphs[g_id], a):
                     self.lastid = g_id
                     return g_id
         self.lastid = -1
         return -1
-    def register(self): # register the last queried graph.
+
+    def register(self):  # register the last queried graph.
         assert self.lastid == -1
         new_id = len(self.graphs)
         if self.lasthash in self.graphhashes:
@@ -68,10 +75,12 @@ class GraphStat():
         self.graphhashes[self.lasthash].append(new_id)
         self.graphs.append(self.lastgraph)
         return new_id
+
     def get(self, id):
         if len(self.graphs) <= id:
             return None
         return self.graphs[id]
+
 
 def unittest(gdb):
     print("unit test.")
@@ -90,9 +99,8 @@ def unittest(gdb):
     if id < 0:
         id = gdb.register()
         print(id)
-    
+
 
 if __name__ == "__main__":
     gdb = GraphStat()
     unittest(gdb)
-
--- graphstat/graphstat_sqlite3.py
+++ graphstat/graphstat_sqlite3.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python
 
-#wrapper for sqlite3.
+# wrapper for sqlite3.
 
 import sqlite3
 import networkx as nx
 from graphstat import encode_graph, decode_graph, matrix_sort, sorteddm, unittest
+
 # debug
 import time
 import logging
 import os
 import sys
 
-class GraphStat():
+
+class GraphStat:
     def __init__(self, filename, create_if_nonexist=True):
         exist = os.path.exists(filename)
         self.conn = sqlite3.connect(filename)
         if not exist and create_if_nonexist:
             c = self.conn.cursor()
-            c.execute('''CREATE TABLE graphs (id integer primary key unique, sdm char(32), graph text)''')
-            c.execute('''CREATE INDEX graphindex on graphs(sdm)''')
+            c.execute(
+                """CREATE TABLE graphs (id integer primary key unique, sdm char(32), graph text)"""
+            )
+            c.execute("""CREATE INDEX graphindex on graphs(sdm)""")
             self.conn.commit()
+
     def __done__(self):
         self.conn.close()
+
     def get(self, id):
         now = time.time()
         cur = self.conn.cursor()
@@ -29,11 +35,12 @@ class GraphStat():
         duration = time.time() - now
         logging.getLogger().debug("  {0} sec SELECT@get()".format(duration))
         return [decode_graph(row[0]) for row in cur]
+
     def query_id(self, g):
         assert nx.is_connected(a)
         logger = logging.getLogger()
         self.lastgraph = g
-        self.lastsdm  = sorteddm(g)
+        self.lastsdm = sorteddm(g)
         # print(self.lastsdm)
         now = time.time()
         cur = self.conn.cursor()
@@ -44,7 +51,7 @@ class GraphStat():
         for row in cur:
             id, g_enc = row
             g_dec = decode_graph(g_enc)
-            if nx.is_isomorphic(g_dec,g):
+            if nx.is_isomorphic(g_dec, g):
                 self.lastid = id
                 return id
             else:
@@ -54,23 +61,28 @@ class GraphStat():
             logger.info("{0} Confliction detected.".format(conflict))
         self.lastid = -1
         return -1
+
     def register(self):
         assert self.lastid == -1
         now = time.time()
         cur = self.conn.cursor()
-        cur.execute("INSERT INTO graphs (id, sdm, graph) VALUES (null, ?, ?)",
-                    (self.lastsdm, encode_graph(self.lastgraph)))
+        cur.execute(
+            "INSERT INTO graphs (id, sdm, graph) VALUES (null, ?, ?)",
+            (self.lastsdm, encode_graph(self.lastgraph)),
+        )
         self.conn.commit()
         duration = time.time() - now
         logging.getLogger().debug("  {0} sec INSERT@register()".format(duration))
         return cur.lastrowid
 
-if __name__  == "__main__":
-    gdb = GraphStat('example.db', create_if_nonexist=True)
-    logging.basicConfig(level=logging.DEBUG,
-                        format="%(asctime)s %(levelname)s %(message)s")
+
+if __name__ == "__main__":
+    gdb = GraphStat("example.db", create_if_nonexist=True)
+    logging.basicConfig(
+        level=logging.DEBUG, format="%(asctime)s %(levelname)s %(message)s"
+    )
     unittest(gdb)
 
-# time genice CRN2  -f voro2 
+# time genice CRN2  -f voro2
 # 15.160 sec MacBook first
 # 46.543 sec         second
